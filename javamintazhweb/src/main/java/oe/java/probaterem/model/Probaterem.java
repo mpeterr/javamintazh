2a0b965cf9fa03a759a531323aa54ce04f85d1cd
mpeterr <m.peter.r@stud.uni-obuda.hu>
2023-05-20 16:10:12 +0200

    Update Probaterem.java
    Probaterem javítása

--- javamintazhweb/src/main/java/oe/java/probaterem/model/Probaterem.java
+++ javamintazhweb/src/main/java/oe/java/probaterem/model/Probaterem.java
@@ -1,20 +1,22 @@
 package oe.java.probaterem.model;
 
+import java.util.Map;
+
 //egy idopontok mezőben a terem időpontjai tárolja.
 //Konstruktorban vegye fel a lehetséges időpontokat (azonosítóval, címkével): (pl. „1”, „Hétfő 18:00-20:00”), ..., („5”, „Péntek 18:00-20:00”)).
 //Megjegyzés: a próbaterem foglaltságát a web alkalmazás teljes futása során nyilván kell tartani (valamilyen application scope vagy singleton megoldással).
 public class Probaterem {
-    private String idopontok;
+    private Map<String, String> idopontok;
 
-    public String getIdopontok() {
+    public Map<String, String> getIdopontok() {
         return idopontok;
     }
 
-    public void setIdopontok(String idopontok) {
+    public void setIdopontok(Map<String, String> idopontok) {
         this.idopontok = idopontok;
     }
 
-    public Probaterem(String idopontok) {
-        this.idopontok = idopontok;
+    public Probaterem(Idopont idopont) {
+        idopontok.put(idopont.getAzonosito(),idopont.getCimke());
     }
 }
