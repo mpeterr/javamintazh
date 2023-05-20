c244b3f1c8f6601e231ec5f537f3e83652710d72
mpeterr <m.peter.r@stud.uni-obuda.hu>
2023-05-20 16:23:09 +0200

    Update Probaterem.java
    Próbaterem feladathoz igazítása

--- javamintazhweb/src/main/java/oe/java/probaterem/model/Probaterem.java
+++ javamintazhweb/src/main/java/oe/java/probaterem/model/Probaterem.java
@@ -1,22 +1,21 @@
 package oe.java.probaterem.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //egy idopontok mezőben a terem időpontjai tárolja.
 //Konstruktorban vegye fel a lehetséges időpontokat (azonosítóval, címkével): (pl. „1”, „Hétfő 18:00-20:00”), ..., („5”, „Péntek 18:00-20:00”)).
 //Megjegyzés: a próbaterem foglaltságát a web alkalmazás teljes futása során nyilván kell tartani (valamilyen application scope vagy singleton megoldással).
 public class Probaterem {
-    private Map<String, String> idopontok;
+    private List<Idopont> idopontok = new ArrayList<>();
 
-    public Map<String, String> getIdopontok() {
-        return idopontok;
-    }
-
-    public void setIdopontok(Map<String, String> idopontok) {
-        this.idopontok = idopontok;
-    }
-
-    public Probaterem(Idopont idopont) {
-        idopontok.put(idopont.getAzonosito(),idopont.getCimke());
+    public Probaterem() {
+        idopontok.add(new Idopont("1", "Hétfő 18:00-20:00", null));
+        idopontok.add(new Idopont("2", "Kedd 18:00-20:00", null));
+        idopontok.add(new Idopont("3", "Szerda 18:00-20:00", null));
+        idopontok.add(new Idopont("4", "Csütörtök 18:00-20:00", null));
+        idopontok.add(new Idopont("5", "Péntek 18:00-20:00", null));
     }
 }
