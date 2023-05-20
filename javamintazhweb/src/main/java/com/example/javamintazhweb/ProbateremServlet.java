e42b536168b3e7bf7987b47d739d78b0eaa83565
mpeterr <m.peter.r@stud.uni-obuda.hu>
2023-05-20 15:21:04 +0200

    Create ProbateremServlet.java

---
+++ javamintazhweb/src/main/java/com/example/javamintazhweb/ProbateremServlet.java
@@ -0,0 +1,19 @@
+package com.example.javamintazhweb;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ProbateremServlet", value = "/ProbateremServlet")
+public class ProbateremServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
